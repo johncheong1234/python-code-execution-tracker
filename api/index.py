9fbc869b32187b5cfad24714b5c63ec460a9474f
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-04-06 11:31:26 +0800

    comment out plus new function

--- api/index.py
+++ api/index.py
@@ -16,81 +16,85 @@ def hello_world():
 @app.route('/submit-cp', methods=['POST'])
 def submitCp():
     data = request.get_json()
+
+# @app.route('/submit-cp', methods=['POST'])
+# def submitCp():
+#     data = request.get_json()
     
-    csvFileName = "algoData"+str(uuid.uuid4())+".csv"
-    # create new python file
-    with open('newFile.py', 'w') as f:
+#     csvFileName = "algoData"+str(uuid.uuid4())+".csv"
+#     # create new python file
+#     with open('newFile.py', 'w') as f:
         
-        f.write('import sys \n')
-        f.write('import io \n')
-        f.write('import opcode\n')
-        f.write('import uuid \n')
-        f.write('import csv \n')
-        f.write('import json \n')
-        f.write('csvFileName =')
-        f.write('\"'+csvFileName+'\"')
-        f.write(' \n')
-        f.write('with open (csvFileName, \'w\', newline=\'\') as f: \n')
-        f.write('    fieldnames = [\'event\', \'arg\', \'line\', \'lasti\', \'opcode\', \'localObjects\'] \n')
-        f.write('    writer = csv.writer(f) \n')
-        f.write('    writer.writerow(fieldnames) \n')
-        f.write('def show_trace(frame, event, arg):\n')
-        f.write('    frame.f_trace_opcodes = True\n')
-        f.write('    code = frame.f_code\n')
-        f.write('    offset = frame.f_lasti\n')
-        f.write('    print(f"| {event:10} | {str(arg):>4} |", end=\' \')\n')
-        f.write('    print(f"{frame.f_lineno:>4} | {frame.f_lasti:>6} |", end=\' \')\n')
-        f.write('    print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} |")\n')
-        f.write('    localObjects = {} \n')
-        f.write('    for key, value in frame.f_locals.items(): \n')
-        f.write('       localObjects[key] = str(value) \n')
-        f.write('    localObjects = json.dumps(localObjects) \n')
-        f.write('    with open(csvFileName, \'a\', newline=\'\') as f: \n')
-        f.write('       writer = csv.DictWriter(f, fieldnames=fieldnames) \n')
-        f.write('       writer.writerow({\'event\': event, \'arg\': arg, \'line\': frame.f_lineno, \'lasti\': frame.f_lasti, \'opcode\': opcode.opname[code.co_code[offset]], \'localObjects\': localObjects}) \n') 
-        f.write('    return show_trace\n')
-        f.write('user_input = "')
-        # split data['testCase'] based on line breaks
-        testCaseSplit = data['testCase'].splitlines()
-        testCaseString = ''
-        # add each line to user_input
-        for line in testCaseSplit:
-            testCaseString += line + '\\n'
-        f.write(testCaseString)
-        f.write('" \n')
-        f.write('saved_stdin = sys.stdin \n')
-        f.write('sys.stdin = io.StringIO(user_input) \n')
-        f.write('sys.settrace(show_trace) \n')
-        # convert data['code'] into a function and write it to file
-        f.write('def newFunction():\n')
-        codeLines = data['code'].splitlines()
-        for line in codeLines:
-            f.write('    ' + line + '\n')
-        f.write('newFunction() \n')
-        f.write('\n')
-        f.write('sys.settrace(None) \n')
-        f.write('sys.stdin = saved_stdin \n')
+#         f.write('import sys \n')
+#         f.write('import io \n')
+#         f.write('import opcode\n')
+#         f.write('import uuid \n')
+#         f.write('import csv \n')
+#         f.write('import json \n')
+#         f.write('csvFileName =')
+#         f.write('\"'+csvFileName+'\"')
+#         f.write(' \n')
+#         f.write('with open (csvFileName, \'w\', newline=\'\') as f: \n')
+#         f.write('    fieldnames = [\'event\', \'arg\', \'line\', \'lasti\', \'opcode\', \'localObjects\'] \n')
+#         f.write('    writer = csv.writer(f) \n')
+#         f.write('    writer.writerow(fieldnames) \n')
+#         f.write('def show_trace(frame, event, arg):\n')
+#         f.write('    frame.f_trace_opcodes = True\n')
+#         f.write('    code = frame.f_code\n')
+#         f.write('    offset = frame.f_lasti\n')
+#         f.write('    print(f"| {event:10} | {str(arg):>4} |", end=\' \')\n')
+#         f.write('    print(f"{frame.f_lineno:>4} | {frame.f_lasti:>6} |", end=\' \')\n')
+#         f.write('    print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} |")\n')
+#         f.write('    localObjects = {} \n')
+#         f.write('    for key, value in frame.f_locals.items(): \n')
+#         f.write('       localObjects[key] = str(value) \n')
+#         f.write('    localObjects = json.dumps(localObjects) \n')
+#         f.write('    with open(csvFileName, \'a\', newline=\'\') as f: \n')
+#         f.write('       writer = csv.DictWriter(f, fieldnames=fieldnames) \n')
+#         f.write('       writer.writerow({\'event\': event, \'arg\': arg, \'line\': frame.f_lineno, \'lasti\': frame.f_lasti, \'opcode\': opcode.opname[code.co_code[offset]], \'localObjects\': localObjects}) \n') 
+#         f.write('    return show_trace\n')
+#         f.write('user_input = "')
+#         # split data['testCase'] based on line breaks
+#         testCaseSplit = data['testCase'].splitlines()
+#         testCaseString = ''
+#         # add each line to user_input
+#         for line in testCaseSplit:
+#             testCaseString += line + '\\n'
+#         f.write(testCaseString)
+#         f.write('" \n')
+#         f.write('saved_stdin = sys.stdin \n')
+#         f.write('sys.stdin = io.StringIO(user_input) \n')
+#         f.write('sys.settrace(show_trace) \n')
+#         # convert data['code'] into a function and write it to file
+#         f.write('def newFunction():\n')
+#         codeLines = data['code'].splitlines()
+#         for line in codeLines:
+#             f.write('    ' + line + '\n')
+#         f.write('newFunction() \n')
+#         f.write('\n')
+#         f.write('sys.settrace(None) \n')
+#         f.write('sys.stdin = saved_stdin \n')
 
-    # run newFile.py
-    os.system('python3 newFile.py')
+#     # run newFile.py
+#     os.system('python3 newFile.py')
 
-    visualList = []
-    codeLines = data['code'].splitlines()
-    with open(csvFileName, 'r') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            # check if the line number is valid
-            if 31<int(row['line']):
-                # codeLinePrior = codeLines[int(row['line'])-3]
-                codeLineAt = codeLines[int(row['line'])-32]
-                row['codeLineAt'] = codeLineAt
-                # row['codeLinePrior'] = codeLinePrior
-                # convert row['localObjects'] string to dict
-                # row['localObjects'] = json.loads(p.sub('\"', row['localObjects']))
-            visualList.append(row)
+#     visualList = []
+#     codeLines = data['code'].splitlines()
+#     with open(csvFileName, 'r') as f:
+#         reader = csv.DictReader(f)
+#         for row in reader:
+#             # check if the line number is valid
+#             if 31<int(row['line']):
+#                 # codeLinePrior = codeLines[int(row['line'])-3]
+#                 codeLineAt = codeLines[int(row['line'])-32]
+#                 row['codeLineAt'] = codeLineAt
+#                 # row['codeLinePrior'] = codeLinePrior
+#                 # convert row['localObjects'] string to dict
+#                 # row['localObjects'] = json.loads(p.sub('\"', row['localObjects']))
+#             visualList.append(row)
 
 
-    return {'visualList': visualList}
+#     return {'visualList': visualList}
 
 if __name__ == '__main__':
     app.run()
