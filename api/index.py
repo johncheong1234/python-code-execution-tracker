ae792dabbd5d98dd17e7582e579f4cd8bf504312
johncheong1234 <john.cheong.2019@smu.edu.sg>
2023-04-06 11:49:25 +0800

    no new py file

--- api/index.py
+++ api/index.py
@@ -5,6 +5,9 @@ import os
 import json
 import re
 import uuid 
+import io
+import sys
+import opcode
 
 app = Flask(__name__)
 CORS(app)
@@ -16,6 +19,66 @@ def hello_world():
 @app.route('/submit-cp', methods=['POST'])
 def submitCp():
     data = request.get_json()
+    csvFileName = "algoData"+str(uuid.uuid4())+".csv"
+    with open (csvFileName, 'w', newline='') as f:
+        fieldnames = ['event', 'arg', 'line', 'lasti', 'opcode', 'localObjects']
+        writer = csv.writer(f)
+        writer.writerow(fieldnames)
+    def show_trace(frame, event, arg):
+        frame.f_trace_opcodes = True
+        code = frame.f_code
+        offset = frame.f_lasti
+        print(f"| {event:10} | {str(arg):>4} |", end=' ')
+        print(f"{frame.f_lineno:>4} | {frame.f_lasti:>6} |", end=' ')
+        print(f"{opcode.opname[code.co_code[offset]]:<18} | {str(frame.f_locals):<35} |")
+        localObjects = {} 
+        for key, value in frame.f_locals.items(): 
+            localObjects[key] = str(value) 
+        localObjects = json.dumps(localObjects) 
+        with open(csvFileName, 'a', newline='') as f: 
+            writer = csv.DictWriter(f, fieldnames=fieldnames) 
+            writer.writerow({'event': event, 'arg': arg, 'line': frame.f_lineno, 'lasti': frame.f_lasti, 'opcode': opcode.opname[code.co_code[offset]], 'localObjects': localObjects}) 
+        return show_trace
+
+    testCaseSplit = data['testCase'].splitlines()
+    testCaseString = ''
+    for line in testCaseSplit:
+        testCaseString += line + '\n' 
+    user_input = testCaseString
+    saved_stdin = sys.stdin
+    sys.stdin = io.StringIO(user_input)
+    sys.settrace(show_trace)
+    functionString = "def newFunction():\n"
+    codeLines = data['code'].splitlines()
+    for line in codeLines:
+        functionString += '    '+line + '\n'
+
+    functionString += 'newFunction()'
+    exec(functionString)
+
+    # print(functionString)
+    sys.stdin = saved_stdin
+    sys.settrace(None)
+    visualList = []
+    codeLines = data['code'].splitlines()
+    with open(csvFileName, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            # check if the line number is valid
+            if 51<int(row['line']):
+                # codeLinePrior = codeLines[int(row['line'])-3]
+                codeLineAt = codeLines[int(row['line'])-52]
+                row['codeLineAt'] = codeLineAt
+                # row['codeLinePrior'] = codeLinePrior
+                # convert row['localObjects'] string to dict
+                # row['localObjects'] = json.loads(p.sub('\"', row['localObjects']))
+            visualList.append(row)
+
+    os.remove(csvFileName)  
+    return {'visualList': visualList}
+
+    
+
 
 # @app.route('/submit-cp', methods=['POST'])
 # def submitCp():
@@ -35,7 +98,7 @@ def submitCp():
 #         f.write('\"'+csvFileName+'\"')
 #         f.write(' \n')
 #         f.write('with open (csvFileName, \'w\', newline=\'\') as f: \n')
-#         f.write('    fieldnames = [\'event\', \'arg\', \'line\', \'lasti\', \'opcode\', \'localObjects\'] \n')
+#         f.write('      fieldnames = [\'event\', \'arg\', \'line\', \'lasti\', \'opcode\', \'localObjects\'] \n')
 #         f.write('    writer = csv.writer(f) \n')
 #         f.write('    writer.writerow(fieldnames) \n')
 #         f.write('def show_trace(frame, event, arg):\n')
